3873962b13d142d0135e62fea0db06c63d4181d2
Maysa Gonçalves <maysagoncalves2003@gmail.com>
2024-09-11 21:25:06 -0300

    tipos inteiros

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = -7;
+        long numero2 = 7;
+
+        uint numero3 = 7; //inteiro sem sinal, apenas numeros positivos
+
+        Console.WriteLine(numero);
+
+    }
+}
