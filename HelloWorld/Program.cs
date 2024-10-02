add4d9d26e00105fa4e5beb3d5055eafb17dc20f
Maysa Gonçalves <maysagoncalves2003@gmail.com>
2024-10-02 18:00:21 -0300

    finalizando exercicio

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Exercício 1:
 
+using System.Text.RegularExpressions;
+
 class Program
 {
     static void Main(string[] args)
@@ -37,5 +39,46 @@ class Program
         double media = (numeral + numeral2) / 2;
         Console.WriteLine($"Média: {media}");
 
+        Console.WriteLine("Digite suas palavras");
+
+        string palavraUser = Console.ReadLine();
+
+        int numeroDeCaracteres = palavraUser.Length;
+
+        Console.WriteLine(numeroDeCaracteres);
+
+        Console.WriteLine("Digite a placa do veículo:");
+
+        
+        string placa = Console.ReadLine();
+
+        string padraoPlaca = @"^[a-zA-Z]{3}[0-9]{4}$";
+
+        bool placaValida = Regex.IsMatch(placa, padraoPlaca);
+
+        if (placaValida)
+        {
+            Console.WriteLine("Verdadeiro");
+        }
+        else
+        {
+            Console.WriteLine("Falso");
+        }
+        DateTime dataAtual = DateTime.Now;
+
+        Console.WriteLine("Data completa:");
+        Console.WriteLine(dataAtual.ToString("dddd, dd 'de' MMMM 'de' yyyy HH:mm:ss"));
+        Console.WriteLine();
+
+        Console.WriteLine("Data no formato dd/MM/yyyy:");
+        Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+        Console.WriteLine();
+
+        Console.WriteLine("Hora no formato 24 horas:");
+        Console.WriteLine(dataAtual.ToString("HH:mm"));
+        Console.WriteLine();
+
+        Console.WriteLine("Data com o mês por extenso:");
+        Console.WriteLine(dataAtual.ToString("dd 'de' MMMM 'de' yyyy"));
     }
 }
